399452b1fe6da62fe6369cef6839780707c24549
Palash Nath <palashnath880@gmail.com>
2025-08-23 01:48:40 +0600

    CartDrawer component updated

--- src/components/shared/CartDrawer.tsx
+++ src/components/shared/CartDrawer.tsx
@@ -11,16 +11,30 @@ import {
 import { Button } from "../ui/button";
 import { BrushCleaning, ShoppingCart, X } from "lucide-react";
 import Link from "next/link";
-import CartItem from "./CartItem";
 import { useCartStore } from "@/store/cart.store";
 import { Badge } from "../ui/badge";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
+import { Product } from "@/interfaces/product";
 
+// CartItem component props
+type CartItemProps = {
+  product: Product;
+  quantity: number;
+  id: string;
+  user_id: string;
+};
+
+/**
+ * Cart Drawer component
+ * @returns
+ */
 export default function CartDrawer() {
   // cart store
   const { trigger, isOpen, items } = useCartStore((state) => state);
 
   return (
-    <Drawer open={isOpen} direction="right" handleOnly dismissible={false}>
+    <Drawer open={isOpen} direction="right" onOpenChange={trigger}>
       <DrawerContent>
         <DrawerHeader className="border-b !border-gray-300">
           <DrawerTitle>
@@ -31,9 +45,9 @@ export default function CartDrawer() {
           </DrawerTitle>
         </DrawerHeader>
         <div className="flex-1 overflow-auto">
-          <div className="flex flex-col gap-5 px-4 py-5">
-            {items.map((_, index) => (
-              <CartItem key={index} />
+          <div className="flex flex-col gap-3 px-4 py-5">
+            {items.map((item) => (
+              <CartItem key={item.id} {...item} />
             ))}
           </div>
         </div>
@@ -63,5 +77,37 @@ export default function CartDrawer() {
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
+  );
+}
+
+/**
+ * Cart Drawer Single Cart Item Component
+ * @returns
+ */
+export function CartItem({ id, product, quantity }: CartItemProps) {
+  return (
+    <Card className="!py-3 !rounded-lg">
+      <CardContent className="flex gap-3 items-start !px-3">
+        <div className="aspect-[16/11] overflow-hidden w-28">
+          <Image
+            src={product.img_url}
+            width={130}
+            height={130}
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col gap-1 flex-1">
+          <h6 className="text-sm font-semibold">{product.title}</h6>
+          <div className="flex gap-2 text-muted-foreground italic text-sm">
+            <span>${product.price.base}</span>
+            <span>X</span>
+            <span>{quantity}</span>
+            <span>=</span>
+            <b>${(product.price.base * quantity).toFixed(2)}</b>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
