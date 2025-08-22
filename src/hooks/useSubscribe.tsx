bf0a163d69d5a56cb6034cc401433d20ebf8dc64
Palash Nath <palashnath880@gmail.com>
2025-08-23 00:27:23 +0600

    useSubscribe hook added

---
+++ src/hooks/useSubscribe.tsx
@@ -0,0 +1,51 @@
+import { useRef, useState } from "react";
+import { toast } from "sonner";
+import z from "zod";
+
+// email validation schema
+const emailSchema = z.email();
+
+/**
+ * useSubscribe hook
+ * Handle subscribe input
+ * @returns
+ */
+export default function useSubscribe() {
+  // state
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // input
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  // subscribe function
+  const subscribe = async () => {
+    // if input not found
+    if (!inputRef.current) return;
+
+    // input value
+    const value = inputRef.current.value;
+
+    // if value is empty
+    if (!value) return;
+
+    const { data, error } = await emailSchema.safeParseAsync(value);
+    if (error) {
+      toast.error("Sorry! Invalid email address.");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      toast.success("Great choice!", {
+        description: "You’ve successfully joined our list.",
+      });
+      setLoading(false);
+      inputRef.current.value = "";
+    } catch (err: any) {
+      setLoading(false);
+      toast.error("Oops! Something went wrong. Please try again.");
+    }
+  };
+
+  return { loading, subscribe, inputRef };
+}
