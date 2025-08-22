0e97c2cd5435863cf0a5ce7108e5a5be9224fd9f
Palash Nath <palashnath880@gmail.com>
2025-08-23 00:30:00 +0600

    useSubscribe hook add in the offers page Newsletter component

--- src/components/offers/Newsletter.tsx
+++ src/components/offers/Newsletter.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import React from "react";
 import { Card, CardContent } from "../ui/card";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import useSubscribe from "@/hooks/useSubscribe";
 
 export default function Newsletter() {
+  // useSubscribe hook
+  const { inputRef, loading, subscribe } = useSubscribe();
+
   return (
     <Card
       className="border-primary/30 bg-repeat-x bg-contain"
@@ -23,8 +29,16 @@ export default function Newsletter() {
             type="email"
             placeholder="Enter your email"
             className="flex-1 border-primary/50"
+            ref={inputRef}
+            disabled={loading}
           />
-          <Button>Subscribe</Button>
+          <Button
+            className="cursor-pointer"
+            onClick={subscribe}
+            disabled={loading}
+          >
+            Subscribe
+          </Button>
         </div>
       </CardContent>
     </Card>
