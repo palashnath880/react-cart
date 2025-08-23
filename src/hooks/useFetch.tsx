1dcefdee40c2a061f6ead5b7f3506ddc44bd01f7
Palash Nath <palashnath880@gmail.com>
2025-08-24 00:33:39 +0600

    useFetch hook added

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+/**
+ * useFetch hook
+ * @returns
+ */
+
+type UseFetch = {
+  error?: () => void;
+  method?: "POST" | "GET" | "DELETE" | "PUT" | "PATCH";
+};
+
+export default function useFetch<T>(params: UseFetch) {
+  const { error, method = "GET" } = params;
+
+  // state
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<T | null>(null);
+
+  const fetcher = async (url: string) => {
+    setLoading(true);
+    const res = await fetch(url, {
+      method,
+    });
+    if (!res.ok) {
+      typeof error === "function" && error();
+      return;
+    }
+    setLoading(false);
+    setData(await res.json());
+  };
+
+  return { loading, fetch: fetcher, data };
+}
